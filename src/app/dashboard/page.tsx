6e8a1ee295114dca59af777e8be50659b565124a
Alessandro Di Bartolomeo <alessandro.dibartolomeo@eqproject.it>
2024-02-05 20:00:32 +0100

    first mongodb attempt

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,25 @@
+import {MongoClient} from "mongodb"
 export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>;
+  async function createData(formData: FormData) {
+    "use server"
+    const rawFormData = {
+      name: formData.get("name")
+    }
+
+    if(process.env.NEXT_PUBLIC_MONGODB_URI != null) {
+      const client = new MongoClient(process.env.NEXT_PUBLIC_MONGODB_URI)
+      client.db("DnD_Project")
+    }
+
+    console.log(process.env.NEXT_PUBLIC_MONGODB_URI, "asdas")
+  }
+  
+  return (<div>
+    <h1>Dashboard Page!</h1>
+    <form action={createData}>
+
+    <input name="name"></input>
+    <button type="submit">Aggiungi</button>
+    </form>
+    </div>);
 }
